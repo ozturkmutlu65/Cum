8c301ffb2b3a30e4072e759d19a8841421489249
Ali <104919540+olurunabirak@users.noreply.github.com>
2024-12-28 17:11:43 +0300

    Update bot.py

--- ArchMusic/core/bot.py
+++ ArchMusic/core/bot.py
@@ -59,8 +59,8 @@ class ArchMusic(Client):
             self.username = get_me.username
             self.id = get_me.id
 
-            video_url = "https://envs.sh/7cB.mp4"
-            caption = "__kumsal aktif ✅. . . ⚡️__"
+            video_url = "https://telegra.ph/file/36221d40afde82941ffff.mp4"
+            caption = "__ bot aktif ✅. . . __"
             
             try:
                 await self.send_video(
